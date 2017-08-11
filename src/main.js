a1ea5591cdeda6587b96245c85c12b5c1dfa8e35
fandepeng <sss_fan@126.com>
2017-08-11 22:24:20 +0800

    home

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import 'common/stylus/variable.styl'
+import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
+
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,38 @@
 /**
  * Created by Administrator on 2017/8/11.
  */
+
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Search from 'components/search/search'
+import Rank from 'components/rank/rank'
+
+Vue.use(Router)
+
+export default new Router({
+  routes:[
+    {
+      path: '/',
+      redirect:'/recommend'
+    },
+    {
+     path: '/recommend',
+     component:Recommend
+    },
+    {
+      path: '/singer',
+      component:Singer
+    },
+    {
+      path: '/rank',
+      component:Rank
+    },
+    {
+      path: '/search',
+      component:Search
+    }
+
+  ]
+})
